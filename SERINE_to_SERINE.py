75e90588555367cd69abe11bfea7ed7279705478
Alexander Lucaci <43097993+aglucaci@users.noreply.github.com>
2019-07-19 17:33:47 -0400

    Add files via upload

---
+++ SERINE_to_SERINE.py
@@ -0,0 +1,216 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 24 16:02:46 2019
+
+@author: alexander lucaci
+
+Grab site substitution data to be used to create circos plot.
+
+http://mkweb.bcgsc.ca/tableviewer/
+https://plot.ly/create/#/
+"""
+# =============================================================================
+# Imports
+# =============================================================================
+import json, csv, sys, os
+import pandas as pd
+import numpy as np
+
+# =============================================================================
+# Declares
+# =============================================================================
+codon_change = {}
+pvalue_threshold = 0.005
+file_passed_threshold = 0
+num_ER_thresholded_sites = 0
+
+#path = "/Users/user/Documents/Pond Lab/Triple hits/Analysis/selectome_trip_ammended_analysis"
+#path = r"E:\SELECTOME_TRIP_AMMENDED_SRV\SELECTOME_TRIP_AMMENDED_SRV_FITTER_JSON"
+#path = r"E:\BUSTED_SIM_SRV_FITTER_JSON\BUSTED_SIM_SRV_FITTER_JSON"
+path = "/Users/alex/Documents/TRIPLE_HITS/SELECTOME_TRIP_AMMENDED_SRV_FITTER_JSON"
+
+files = [path+"/"+f.name for f in os.scandir(path) if f.name.endswith(".json")]
+#files = ["/Users/user/Documents/Pond Lab/Triple hits/Analysis/selectome_trip_ammended_analysis/ENSGT00670000097768.Euteleostomi.003.nex.FITTER.json"]
+
+TH_Threshold = 3
+# =============================================================================
+# Helper funcs.
+# =============================================================================
+def diff_count(from_codon, to_codon):
+    count = 0
+    #print(from_codon, to_codon)
+    if from_codon[0] != to_codon[0]: count += 1
+    if from_codon[1] != to_codon[1]: count += 1
+    if from_codon[2] != to_codon[2]: count += 1
+    #print(type(from_codon), type(to_codon))
+    #print(from_codon.difference(to_codon))
+    return count
+
+def subprocessing_thisrow(this_row):
+    global codon_change
+    for k in this_row[0]:
+        for keys in this_row[0][k]:
+            for to_codon in this_row[0][k][keys]:
+                from_codon = keys
+                if diff_count(from_codon, to_codon) == 3:
+                    #print("->", filename, from_codon, to_codon)
+                    change = {to_codon : len(this_row[0][k][from_codon][to_codon])} #counts   
+                    if from_codon not in codon_change:
+                        codon_change[from_codon] = change
+                    else:
+                        if to_codon not in codon_change[from_codon]:
+                            codon_change[from_codon][to_codon] = len(this_row[0][k][from_codon][to_codon])
+                        else:
+                            codon_change[from_codon][to_codon] += len(this_row[0][k][from_codon][to_codon])
+
+def writetofile(filename, data, mode):
+    #print(filename, data, mode)
+    with open(filename, mode) as f:
+        f.write(data + "\n")
+    f.close()
+    
+
+def read_json_fullfilter(filename): #Pvalue and ER Thresholdings for sensitive sites
+    global codon_change, file_passed_threshold, num_ER_thresholded_sites, TH_Threshold, pvalue_threshold
+    this_row = []
+    with open(filename, "r") as fh:
+        json_data = json.load(fh)
+        THvsDH_LRT_pvalue = json_data["test results"]["Triple-hit vs double-hit"]["p-value"]
+        EvidenceRatio_TH = json_data["Evidence Ratios"]["Three-hit"][0]
+        Site_subs = json_data["Site substitutions"]
+        #Filtering
+        if float(THvsDH_LRT_pvalue) >= pvalue_threshold: return #P VALUE THRESHOLDs
+        Threshold_TH = TH_Threshold * np.mean(EvidenceRatio_TH)
+        Filtered_Threshold_TH = list(filter(lambda x: x > Threshold_TH, EvidenceRatio_TH))
+        #this_row.append(json_data["Site substitutions"])
+        if Filtered_Threshold_TH == []: return #NO SITES ABOVE THRESHOLD 
+        num_ER_thresholded_sites += len(Filtered_Threshold_TH) 
+        for i, item in enumerate(EvidenceRatio_TH):
+            if item in Filtered_Threshold_TH:
+                try:
+                    #print(i, "{'" + str(i) + "': " + str(Site_subs[str(i)]) + "}")
+                    this_row.append({i: Site_subs[str(i)]})
+                    #print("witing to file")
+                    #print(file_passed_threshold, filename, Site_subs[str(i)])
+                    
+                    #can do analysis here.
+                except:
+                    #SITE SUB NOT FOUND
+                    #print("ERROR:", i, "{'" + str(i) + "': " + str(Site_subs[str(i)]) + "}")
+                    #print("ERROR:", i, "SITE SUB NOT FOUND")
+                    pass
+        mode = "a+"
+        writetofile("p" + str(pvalue_threshold).replace(".", "-") + "_Threshold_" + str(TH_Threshold) + "x_.txt", filename.split("/")[-1].replace(".FITTER.json", ""), mode)
+        file_passed_threshold += 1
+    fh.close()
+    
+    if len(this_row) == 0: return
+    subprocessing_thisrow(this_row)
+  
+def read_json_p_filter(filename): #Pvalue and ER Thresholdings for sensitive sites
+    global codon_change, file_passed_threshold, num_ER_thresholded_sites
+    this_row = []
+    with open(filename, "r") as fh:
+        json_data = json.load(fh)
+        THvsDH_LRT_pvalue = json_data["test results"]["Triple-hit vs double-hit"]["p-value"]
+        #EvidenceRatio_TH = json_data["Evidence Ratios"]["Three-hit"][0]
+        Site_subs = json_data["Site substitutions"]
+        #Filtering
+        if float(THvsDH_LRT_pvalue) >= pvalue_threshold: return #P VALUE THRESHOLDs
+        this_row.append(Site_subs)
+        writetofile("p" + str(pvalue_threshold).replace(".", "-") + ".txt", filename.split("/")[-1].replace(".FITTER.json", ""), "a+")
+        file_passed_threshold += 1
+    fh.close()
+    
+    if len(this_row) == 0: return
+    subprocessing_thisrow(this_row)
+    
+def read_json_nofilter(filename):
+    global codon_change, file_passed_threshold, num_ER_thresholded_sites
+    this_row = []
+    with open(filename, "r") as fh:
+        json_data = json.load(fh)
+        Site_subs = json_data["Site substitutions"]
+        file_passed_threshold += 1
+    fh.close()
+    this_row.append(Site_subs)
+    writetofile("allfiles.txt", filename.split("/")[-1].replace(".FITTER.json", ""), "a+")
+    if len(this_row) == 0: return
+    subprocessing_thisrow(this_row)
+
+
+# =============================================================================
+# Main subroutine
+# =============================================================================
+def main_sub(output_filename, mode):
+    file_count = 0
+    for file in files:
+        #print(file_count, "Processing:", file)
+        if mode == "NOFILTER": read_json_nofilter(file)
+        if mode == "P_FILTER": read_json_p_filter(file)
+        if mode == "FULLFILTER": read_json_fullfilter(file)
+        
+        file_count += 1
+        
+    #Sum this.
+    triple_changes = 0
+    SERINE_to_SERINE = 0
+    SERINE_CODONS = ["TCT", "TCC", "TCA", "TCG", "AGT", "AGC"]
+    
+    """ This is on triple instant. changes"""
+    for key in codon_change:
+        #print(key, codon_change[key])
+        for second_key in codon_change[key]:
+            #if diff_count(key, second_key) == 3:
+            triple_changes += codon_change[key][second_key]
+            if key in SERINE_CODONS and second_key in SERINE_CODONS: SERINE_to_SERINE += codon_change[key][second_key]
+        
+    print("Number of files:", file_passed_threshold)
+    if mode == "FULLFILTER": print("Number of ER Thresholded Sites:", num_ER_thresholded_sites)
+    print("Total number of triple changes observed:", triple_changes)
+    print("TH: Serine to Serine changesT:", SERINE_to_SERINE)
+    
+
+# =============================================================================
+# Main main
+# =============================================================================
+"""
+print("\n", "() No filter analysis") 
+main_sub("", "NOFILTER")
+
+codon_change, file_passed_threshold, num_ER_thresholded_sites = {}, 0 , 0
+print("\n", "() p < 0.005") 
+main_sub("", "P_FILTER")
+"""
+
+codon_change, file_passed_threshold, num_ER_thresholded_sites = {}, 0 , 0
+print("\n", "() p < 0.005, TH Threshold (3x)") 
+main_sub("", "FULLFILTER")
+
+TH_Threshold = 10
+codon_change, file_passed_threshold, num_ER_thresholded_sites = {}, 0 , 0
+print("\n", "() p < 0.005, TH Threshold (10x)") #REMEMBER TO CHANGE THE THRESHOLD IN THE FUNCTION
+main_sub("", "FULLFILTER")
+
+
+#need to run this three times.
+#different output files
+#differnt modes
+# =============================================================================
+# End of file
+# =============================================================================
+"""
+    if os.path.exists(filePath):
+        os.remove(filePath)
+    else:
+        print("Can not delete the file as it doesn't exists")
+"""
+
+
+
+
+
+
+
+
--- plot_w_and_wo_Serines.py
+++ plot_w_and_wo_Serines.py
@@ -21,104 +21,166 @@ from plotly import tools
 # =============================================================================
 # Declares
 # =============================================================================
-
 #With Serine Fitters (aka the originals)
-fitters = r"E:\SELECTOME_TRIP_AMMENDED_SRV\SELECTOME_TRIP_AMMENDED_SRV_FITTER_JSON"
+#fitters = r"E:\SELECTOME_TRIP_AMMENDED_SRV\SELECTOME_TRIP_AMMENDED_SRV_FITTER_JSON"
+fitters = "/Users/alex/Documents/TRIPLE_HITS/SELECTOME_TRIP_AMMENDED_SRV_FITTER_JSON" #originals
 
 #without Serines, based on the FitMultiMode_cpl.bf and MG_REV_TRIP_serineless.bf
-wo_serine_fitters = r"E:\SELECTOME_TRIP_AMMENDED_SRV\W-O_SERINES_pvalueand10x"
+#wo_serine_fitters = r"E:\SELECTOME_TRIP_AMMENDED_SRV\W-O_SERINES_pvalueand10x"
+wo_serine_fitters = "/Users/alex/Documents/TRIPLE_HITS/WO_SERINE_pvalue0-005_Thres_10x"
 
 #Look for this ending.
-file_ending = "FITTER.json"
+file_ending = ".FITTER.json"
 
-
+filenames_by_class = ["allfiles.txt", "p0-005.txt", "p0-005_Threshold_3x_.txt", "p0-005_Threshold_10x_.txt"]
 # =============================================================================
 # Helper functions
 # =============================================================================
 def diff(b, a): #b is the original, a is the without serine
     return float(b)-float(a)
 
-def plot(x, y, title):
+def plot(x, y, title, output):
     plotly.offline.init_notebook_mode(connected=False)
-    
-    
-    
     data = [
     go.Bar(
         x = x,
         y = y,
         #base = [-500,-600,-700],
-        marker = dict(
-          color = ['red' if val < 0 else 'blue' for val in y]
-        )
-        #name = 'expenses'
-    )]
-    
-    
-    
-    
+        marker = dict(color = ['red' if val < 0 else 'blue' for val in y])#name = 'expenses'
+        )]
     fig = go.Figure(data=data)
-    
     #fig["layout"].update(title="TH Rate differences (with and without Serines) Total cases: Num neg. cases: ")
-    fig["layout"].update(title=title)
-    
+    fig["layout"].update(title=output.replace(".html", "") + " - " + title)
     #fig['layout']['xaxis1'].update(title=xaxislabel)
     fig['layout']['yaxis1'].update(title='TH rate difference (original - without serine)')
-    
-    plotly.offline.plot(fig, filename='pV0-005_Thres10x_diff_THrate.html')
+    plotly.offline.plot(fig, filename=output)
+    #plotly.offline.plot(fig, filename='pV0-005_Thres10x_diff_THrate.html')
 
 def read_json_returnTHrate(filename):
     with open(filename, "r") as fh:
-        json_data = json.load(fh)
+        json_data = json.load(fh, strict=False)
         fh.close()
     return json_data["fits"]["MG94 with double and triple instantaneous substitutions"]["Rate Distributions"]["parameters"]["rate at which 3 nucleotides are changed instantly within a single codon"]
+
+
 
 # =============================================================================
 # Main subroutine
 # =============================================================================
-#plot(['2016','2017','2018'], [-500,-600,-700])
+    
+def main(files):
+    """ Data processing """
+    plot_data_holder = {}                 
+    filecount = 0
+    numnegatives = 0
+    
+    for i, file in enumerate(files):
+        #print("Loading:", i, file)
+        file_a = read_json_returnTHrate(file) # without serines
+        #print(os.path.join(fitters, file.split("\\")[-1]))
+        file_b = read_json_returnTHrate(os.path.join(fitters, file.split("/")[-1])) #original
+        print(i+1, file.split("/")[-1], file_b, file_a, diff(file_b,file_a))
+        plot_data_holder[file.split("/")[-1]] = diff(file_b,file_a) 
+        #if i == 100: break
+        if diff(file_b,file_a) < 0: numnegatives += 1
+        filecount += 1
 
-#ys.exit(1)
+    """ Plotting """
+ 
+    x, y = [], []
+    for key in plot_data_holder.keys():
+         x.append(key)
+         y.append(plot_data_holder[key])
+    print(numnegatives, str((numnegatives/filecount)*100) + "%")
+    asapercent = round((numnegatives/filecount)*100, 2)
+    print(asapercent)
+    title = "TH Rate differences (with and without Serines) Total cases: " + str(filecount) +  " Num neg. cases: " + str(numnegatives) + " (" + str(asapercent) + "%)"
+    plot(x, y, title, output_filename)
+# =============================================================================
+# Main
+# =============================================================================
+by_file_class = True
+print("() Init")
+""" The setup. """
+if by_file_class == False: 
+    #plot(['2016','2017','2018'], [-500,-600,-700])
+    #sys.exit(1)
+    path = wo_serine_fitters
+    #files = [path+"\\"+f.name for f in os.scandir(path) if f.name.endswith(file_ending)]
+    files = [path+"/"+f.name for f in os.scandir(path) if f.name.endswith(file_ending)]
+    #print(", ".join(["#", "Filename", "Original TH rate", "W/O Serine TH Rate", "Difference (Original-W/O)"]))
+    output_filename = "default.html"
+    
+#by file class?
+print("() Processing by file class")
+if by_file_class == True:
+    
+    path = "/Users/alex/Documents/TRIPLE_HITS/WO_SERINES_ALLFILES_FITTERS_JSON"
+    
+    for filename in filenames_by_class:
+        files = []
+        cnt = 0
+        #filename = filenames_by_class[0]
+        output_filename = filename.replace(".txt", ".html")
+        with open(filename) as f:
+            for n, line in enumerate(f):
+                exists = path+"/"+line.strip()+file_ending
+                if os.path.isfile(exists):
+                    #files.append(path+"/"+line.strip()+".FITTER.json")
+                    files.append(exists)
+                    cnt += 1
+            f.close()
+        #print(len(files), cnt)
+    
+        main(files)       
+    
 
-path = wo_serine_fitters
-files = [path+"\\"+f.name for f in os.scandir(path) if f.name.endswith(file_ending)]
+sys.exit(3) # --- ##
 
-print(", ".join(["#", "Filename", "Original TH rate", "W/O Serine TH Rate", "Difference (Original-W/O)"]))
-
+""" Data processing """
 plot_data_holder = {}                 
-numnegatives = 0
 filecount = 0
+numnegatives = 0
 
 for i, file in enumerate(files):
     #print("Loading:", i, file)
     file_a = read_json_returnTHrate(file) # without serines
     #print(os.path.join(fitters, file.split("\\")[-1]))
-    file_b = read_json_returnTHrate(os.path.join(fitters, file.split("\\")[-1])) #original
-    
-    
-    print(i+1, file.split("\\")[-1], file_b, file_a, diff(file_b,file_a))
-    plot_data_holder[file.split("\\")[-1]] = diff(file_b,file_a) 
-    
+    file_b = read_json_returnTHrate(os.path.join(fitters, file.split("/")[-1])) #original
+    print(i+1, file.split("/")[-1], file_b, file_a, diff(file_b,file_a))
+    plot_data_holder[file.split("/")[-1]] = diff(file_b,file_a) 
     #if i == 100: break
     if diff(file_b,file_a) < 0: numnegatives += 1
     filecount += 1
-    
-x, y = [], []
 
+""" Plotting """
+ 
+x, y = [], []
 for key in plot_data_holder.keys():
      x.append(key)
      y.append(plot_data_holder[key])
-     
-
 print(numnegatives, str((numnegatives/filecount)*100) + "%")
-
 asapercent = round((numnegatives/filecount)*100, 2)
-
 print(asapercent)
+title = "TH Rate differences (with and without Serines) Total cases: " + str(filecount) +  " Num neg. cases: " + str(numnegatives) + " (" + str(asapercent) + "%)"
+plot(x, y, title, output_filename)
 
+
+
+"""
+#--- sorted version
+import collections
+sorted_plot_data_holder = collections.OrderedDict(plot_data_holder)
+x, y = [], []
+for key in sorted_plot_data_holder.keys():
+     x.append(key)
+     y.append(sorted_plot_data_holder[key])
+print(numnegatives, str((numnegatives/filecount)*100) + "%")
+asapercent = round((numnegatives/filecount)*100, 2)
+print(asapercent)
 title = "TH Rate differences (with and without Serines) Total cases: " + str(filecount) +  " Num neg. cases: " + str(numnegatives) + " (" + str(asapercent) + "%)"
 plot(x, y, title)
-
+"""
 
 # =============================================================================
 # End of file
