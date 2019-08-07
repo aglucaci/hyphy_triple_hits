4daa380cc54b348a444cc3b1b569a35ee029a1b7
Alexander Lucaci <43097993+aglucaci@users.noreply.github.com>
2019-08-07 19:39:56 -0400

    Add files via upload
    initial commit

--- run_alignstat.R
+++ run_alignstat.R
@@ -1,25 +1,58 @@
-## R script ######
+#NOTES
+#2019
+#Run Alignstat on realigned and reference selectome alignments
+#Alexander Lucaci
+
+
+## IMPORTS
 library("AlignStat")
 
+## DECLARES
 setwd("~/Documents/TRIPLE_HITS/Realignment_testing/AlignStat")
 
-# Example data loading
-#data("../data/reference_alignment")
-#data("../data/comparison_alignment")
-#data("reference_alignment")
-#data("comparison_alignment")
+REF_PATH = "../selectome_aligned_fasta"
+COMP_PATH = "../mafft_changed_filenames"
+OUTPUT_PATH = "results"
 
-ref = "../selectome_aligned_fasta/ENSGT00390000000002.Euteleostomi.002.fasta"
-#comp = "../mafft/ENSGT00390000000002.Euteleostomi.002.fasta"
-comp = "../mafft_changed_filenames/ENSGT00390000000002.Euteleostomi.002.fasta"
+## HELPER FUNCTIONS
+run_alignstat = function(ref, comp, output_filename){
+  print("COMPARING ALIGNMENTS..")
+  PAC = compare_alignments  (ref, comp, CS=TRUE, SP=TRUE)
+  
+  #temp = PAC$sum_of_pairs
+  #cat(temp, "text.txt")
+  print("CREATING DATAFRAME")
+  df <- NULL
+  df$File <- basename(ref)
+  df$SP <- PAC$sum_of_pairs$sum.of.pairs.score
+  df$PS <- PAC$sum_of_pairs$reverse.sum.of.pairs.score
+  df$CS <- PAC$sum_of_pairs$column.score
+  df$Score <- PAC$similarity_score
+  df$CS_score <- PAC$column_score$column.score
+  
+  print("WRITING DATAFRAME TO FILE")
+  write_df(df, output_filename)
+}
 
-# Alignment comparison calculation
-#PAC <- compare_alignments  (reference_alignment, comparison_alignment, CS=TRUE, SP=TRUE)
-PAC <- compare_alignments  (ref, comp, CS=TRUE, SP=TRUE)
+write_df = function(df, filename){
+  write.table(df, file = filename)
+}
 
-# Results visualisation
-plot_similarity_heatmap    (PAC)
-plot_dissimilarity_matrix  (PAC)
-plot_similarity_summary    (PAC, CS=TRUE, cys=TRUE)
-plot_dissimilarity_summary (PAC, stack=TRUE)
-plot_SP_summary            (PAC, CS=TRUE)
+## MAIN SUBROUTINE
+print("STARTING SCRIPT..")
+files <- list.files(path=REF_PATH, pattern="*.fasta", full.names=TRUE, recursive=FALSE)
+
+#paste(COMP_PATH, basename(files[1]), sep="/")
+print("ENTERING FOR LOOP..")
+for (file in files) {
+  print("starting analysis for:")
+  print(ref)
+  ref = file
+  comp = paste(COMP_PATH, basename(ref), sep="/")
+  output_filename = paste(OUTPUT_PATH, paste(basename(ref), ".out", sep=""), sep="/")
+  run_alignstat(ref, comp, output_filename)
+}
+
+
+## END OF FILE
+#https://stackoverflow.com/questions/14958516/looping-through-all-files-in-directory-in-r-applying-multiple-commands
