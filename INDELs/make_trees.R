426bcca7593f333553cf15be647b2e69666aacaf
Alexander Lucaci <43097993+aglucaci@users.noreply.github.com>
2019-12-05 07:52:47 -0500

    Add files via upload

--- INDELs/make_trees.R
+++ INDELs/make_trees.R
@@ -1,10 +1,32 @@
 #http://www.phytools.org/eqg/Exercise_3.2/
+#https://cran.r-project.org/web/packages/ape/ape.pdf
 
+# IMPORTS
 library(ape)
 
+# Declares
+set.seed(24)
+#https://www.rdocumentation.org/packages/compositions/versions/1.40-3/topics/rnorm
+mat <- matrix(abs(rnorm(30, 2, 1)))
+
 ## simulate a phylogeny
-tree <- rtree(n = 30)
+
+# branch lengths drawn from a distribution, see above.
+tree <- rtree(n = 30, rooted = TRUE, br = mat)
+
+# uniform distribution of branch lengths
+#tree <- rtree(n = 30, rooted = TRUE, br = TRUE)
+
+#generic way to make tree
+#tree <- rtree(n = 30, rooted = TRUE)
+
+#plot the tree
 plot(tree, edge.width = 2)
 
-
+#write to file
 write.tree(tree, file = "example.trees")
+
+head(tree)
+
+
+## END OF FILE
