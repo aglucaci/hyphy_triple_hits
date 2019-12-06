16b45024b07244b2208fc3fa07951c5b583d7250
Alexander Lucaci <43097993+aglucaci@users.noreply.github.com>
2019-12-05 19:44:27 -0500

    Add files via upload

---
+++ MIS_NEW_RESULTS/SELECTOME_TH_circos-tableviewer-tbiejzr/batch/createimage.sh
@@ -0,0 +1,10 @@
+PERL=/home/martink/bin/perl
+TABLEVIEWER_DIR=/home/martink/work/circos/svn/tools/tableviewer
+CIRCOS_DIR=/home/martink/work/circos/svn-tags/circos-tableviewer/
+WORKING_DIR=/home/martink/www/htdocs/tableviewer/tmp/tbiejzr
+cd /home/martink/www/htdocs/tableviewer/tmp/tbiejzr
+$PERL $TABLEVIEWER_DIR/bin/parse-table -conf etc/parse-table.conf -file uploads/table.txt -segment_order=ascii,size_desc -placement_order=row,col -interpolate_type count -color_source row -transparency 1 -fade_transparency 0 -ribbon_layer_order=size_asc > data/parsed.txt
+cat data/parsed.txt | $TABLEVIEWER_DIR/bin/make-conf -dir data
+$PERL $CIRCOS_DIR/bin/circos -param random_string=tbiejzr -conf etc/circos.conf
+cd /home/martink/www/htdocs/tableviewer/tmp/tbiejzr; /home/martink/bin/perl /home/martink/work/circos/svn-tags/circos-tableviewer//bin/circos -param random_string=tbiejzr -conf /home/martink/www/htdocs/tableviewer/tmp/tbiejzr/etc/circos.conf 2>&1 > /home/martink/www/htdocs/tableviewer/tmp/tbiejzr/results/report.txt
+tar cvfz circos-tableviewer-tbiejzr.tar.gz *
