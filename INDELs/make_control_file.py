5245dc22280d4b9a2e01ed75a006a1cdb1318ae3
Alexander Lucaci <43097993+aglucaci@users.noreply.github.com>
2019-12-06 19:14:27 -0500

    Add files via upload

--- INDELs/make_control_file.py
+++ INDELs/make_control_file.py
@@ -3,7 +3,9 @@
 """
 Created on Wed Dec  4 19:39:17 2019
 
-@author: user
+@author: Alexander G. Lucaci
+
+Generate codon simulations with indel variation.
 """
 
 # =============================================================================
@@ -46,20 +48,18 @@ control_file.append("  [deletemodel]  POW  1.8 500 //  Power law deletion length
 control_file.append("  [indelrate]    0.1          //  insertion rate = deletion rate = 0.1")
 control_file.append("                              //  relative to average substitution rate of 1.   ")
 control_file.append("")
-control_file.append("[TREE] treename ((((A:0.2, B:0.3):0.3,(C:0.5, D:0.3):0.2):0.3, E:0.7):1.0);")
+#control_file.append("[TREE] treename ((((A:0.2, B:0.3):0.3,(C:0.5, D:0.3):0.2):0.3, E:0.7):1.0);")
+control_file.append("[TREE] treename ((((t22:0.7884912707,((t14:0.8485323223,t20:0.875784308):0.05844755005,((t29:0.9256454913,t8:0.5681589271):0.6796265363,t30:0.2025386344):0.9573877391):0.08610738721):0.921048284,(((t3:0.9725932428,t17:0.4366256092):0.6381316015,((t16:0.3180074089,t27:0.3864220553):0.2001682797,t21:0.5908179402):0.4302841208):0.4914731416,(t11:0.2886261365,t2:0.8246886986):0.2573057537):0.569526993):0.6803977506,(t10:0.06485857069,(t6:0.001371138263,(((t7:0.3931164765,t12:0.07855357323):0.406146232,(t26:0.9711873827,t23:0.8914815376):0.08172181295):0.4853650811,(t9:0.3540119417,t24:0.01268518576):0.5522214433):0.6867216111):0.939245726):0.7275894266):0.8185117426,((t4:0.5687292085,((t15:0.9149642589,t25:0.1793684226):0.3222816291,t5:0.571961625):0.3456264588):0.1211217023,(t1:0.1853761284,((t13:0.4666023802,(t28:0.6691324431,t18:0.7461213013):0.3977537802):0.7462856739,t19:0.4380299225):0.7384211062):0.4651579566):0.9417840841);")
 control_file.append("")
 control_file.append("        ")
 control_file.append("//  User trees are defined here")
 control_file.append("")
 control_file.append("")
 control_file.append("[PARTITIONS] partitionname             //  [PARTITIONS] blocks say which models go with")
-control_file.append("  [treename modelname 1000]            //  which trees and define the length of the")
+control_file.append("  [treename modelname 900]            //  which trees and define the length of the")
 control_file.append("                                       //  sequence generated at the root (1000 here).")
 control_file.append("")
 control_file.append("[EVOLVE] partitionname 1 CODON_INDEL_SIMS_1  //  This will generate 100 replicate datasets ")
-
-
-
 #control_file.append("partitionname 5 outputname2")
 #control_file.append("partitionname 5 outputname3")
 control_file.append("        ")
@@ -78,8 +78,6 @@ num_output_files = 30 #number of files to simulate
 # =============================================================================
 # Main subroutine
 # =============================================================================
-
-
 #Need to generate a new tree
 
 #Loop over indel rates, 0.1, 1.0 in steps of 0.1
@@ -91,7 +89,7 @@ num_output_files = 30 #number of files to simulate
 #Run the indelible binary.
 
 def main(RATE):
-    with open("control.txt", "w") as f:
+    with open("control.txt", "w") as f: #this should rewrite the control.txt each time.
         
         for line in control_file:
             #print(line)
@@ -106,7 +104,7 @@ def main(RATE):
                 #and then continue
                 f.write(line + "\n")
                 for n in range(2, num_output_files + 1):
-                    f.write("partitionname 1 output" + str(n) + "\n")
+                    f.write("partitionname 1 CODON_INDEL_SIMS_" + str(n) + "\n")
                 continue
             
             f.write(line + "\n")
@@ -137,6 +135,7 @@ def main(RATE):
     
 for rate in INDEL_RATE:
     main(rate)
+    #break
 
 # =============================================================================
 #  End of file
--- INDELs/make_trees.R
+++ INDELs/make_trees.R
@@ -1,3 +1,6 @@
+# Generate a newick tree.
+# Author: Alexander G. Lucaci
+
 #http://www.phytools.org/eqg/Exercise_3.2/
 #https://cran.r-project.org/web/packages/ape/ape.pdf
 
@@ -5,20 +8,20 @@
 library(ape)
 
 # Declares
-set.seed(24)
+#set.seed(24)
 #https://www.rdocumentation.org/packages/compositions/versions/1.40-3/topics/rnorm
-mat <- matrix(abs(rnorm(30, 2, 1)))
+mat <- matrix(abs(rnorm(30, 2, 1.5))) # make branch length a distribution, <1 to about 8
 
 ## simulate a phylogeny
 
 # branch lengths drawn from a distribution, see above.
-tree <- rtree(n = 30, rooted = TRUE, br = mat)
+#tree <- rtree(n = 30, rooted = TRUE, br = mat)
 
-# uniform distribution of branch lengths
+# uniform distribution of branch lengthsi
 #tree <- rtree(n = 30, rooted = TRUE, br = TRUE)
 
 #generic way to make tree
-#tree <- rtree(n = 30, rooted = TRUE)
+tree <- rtree(n = 30, rooted = TRUE) #make the n a distribution like 10-50
 
 #plot the tree
 plot(tree, edge.width = 2)
